bdff44736d3872e140b38706dd9c3036bb5da231
Muhammad Alif Akbar <alif.thetricky@gmail.com>
2018-01-03 20:45:59 +0700

    update dari irfan

--- main.py
+++ main.py
@@ -1,39 +1,94 @@
-'''
-MAIN APP FOR MY TA
-'''
-
-import turtle
 import input_reader
 import utils
-from model import MBR
+import time
+from model import Point, RTree
+# import pickle
+import random
 
-def main(file_address):
-    """
-    Main algorithm functions
-    """
+def build_tree(file_dir):    
+     voronoi_list = input_reader.read_input(file_dir)
+     tree = RTree(max_content_size = 4)
 
-    # read input from raw data
-    # voronoi_list = input_reader.read_input(file_address)
-    
-    # store readed voronoi to increase processing speed
-    # input_reader.store_processed_input(voronoi_list)
+     startConstruct = int(round(time.time()*1000))
+     for voronoi in voronoi_list:
+        #  print("insert region:", voronoi.name)
+         tree.insert(voronoi)
+        #  tree.reset()
+        #  tree.inspectTree()
+        #  print(tree.choose_leaf())
+     endConstruct = int(round(time.time()*1000))
+     longConstruct = endConstruct-startConstruct
+    #  print (longConstruct,'Time Construct (ms)')
 
-    # read input from processed data
-    voronoi_list = input_reader.get_processed_input(file_address)
+    #  with open('save/pypy_tree.pkl', 'wb') as file_output:
+    #      pickle.dump(tree, file_output, pickle.HIGHEST_PROTOCOL)
 
-    # create MBR
-    mbr = MBR()
-    for voronoi in voronoi_list:
-        mbr.add(voronoi)
+    #  with open('save/pypy_tree.pkl', 'rb') as file_input:
+    #    tree = pickle.load(file_input)
+     return tree
 
-    # new query point
-    # query_point = Point(0.2221, 2.2222)
-    # voronoi_position = mbr.find_voronoi_for_point(mbr, query_point)
-
-    print('total voronoi', len(voronoi_list))
-    # print('my point location', query_point)
 
 if __name__ == '__main__':
-    for a_file in utils.get_all_files_in('test/'):
-        draw_voronoi(a_file)
-        break
+    #file_input = raw_input('Input File (file.input): ')
+    # file_input = 'test/test-irfan.input'
+    file_input = 'object/region-10-titik.input'
+        
+    tree= build_tree(file_input)
+    tree.inspectTree()
+
+
+    # with open('save/pypy_seq-05-titik.pkl', 'wb') as file_output:
+    #     pickle.dump(tree, file_output, pickle.HIGHEST_PROTOCOL)
+
+    # for v in voronoi_list:
+    #     print(v.name)
+    # points = [  Point(), #R0
+    #             Point(1.000, 198),#R14
+    #             Point(662.1,551.96)]#R8
+    # for p in points:
+
+    # for v in voronoi_list;
+
+    # random.uniform(1.0,)
+    """
+    Input query point
+    """
+
+    query_point =Point(465,484)
+
+    """
+    R-Tree Search
+    
+    """
+    # startRtree = int(round(time.time() * 1000))
+    # region = tree.search(query_point, include_on_edge=True)
+    # if region:
+    #     print ('Region Found:', region)
+    # else:
+    #     print ('Region Not Found')
+
+    # endRtree = int(round(time.time() * 1000))
+    # longRtree = endRtree - startRtree
+    # print ('Waktu Pencarian R-Tree = ', longRtree, 'ms')
+    
+    # #
+    # #      # query_point = Point([float(data) for data in raw_input('Query Point (x,y): ').split(',')])
+    # #
+    # #
+
+    """
+    Sequential Search
+    
+    """
+    voronoi_list = input_reader.read_input(file_input) 
+    startSeq = int(round(time.time()*1000))
+    
+    response = tree.seq_search(query_point,True, voronoi_list)
+    print(response)
+    p = tree.search(query_point,True)
+    # print(p)
+    
+    endSeq=int(round(time.time()*1000))
+    longSeq = endSeq - startSeq
+    
+    print ('Waktu Pencarian Linear = ', longSeq, 'ms')
