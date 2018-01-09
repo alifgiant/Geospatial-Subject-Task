3cfd48c21c8f9c6068083d455fecc501eef8dedc
Muhammad Alif Akbar <alif.thetricky@gmail.com>
2018-01-09 21:57:37 +0700

    add test case reader

---
+++ input_reader_test.py
@@ -0,0 +1,20 @@
+"""
+Test Input Reader
+"""
+
+import unittest
+from input_reader import read_input
+
+class TestInputReader(unittest.TestCase):
+    """
+    Test Class
+    """
+    def test_read_input(self):
+        data = read_input("test/region-4/region-4.input")
+        self.assertEqual(len(list(data)), 4)
+
+        data = read_input("test/region-15/region-15.input")
+        self.assertEqual(len(list(data)), 15)
+
+if __name__ == '__main__':
+    unittest.main()
