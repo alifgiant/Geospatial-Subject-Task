9d528455f3ccbca09f82fb0f2ae01947dd5e6ee2
Muhammad Alif Akbar <alif.thetricky@gmail.com>
2018-01-09 22:58:57 +0700

    re-code build tree to make it efficient

--- main.py
+++ main.py
@@ -7,60 +7,46 @@ import pickle
 import os.path
 from model import Point, RTree
 import input_reader
-import utils
 
-def build_tree(voronoi_file_source, should_build_new = False):
+def build_tree(voronoi_file_source, should_build_new=False):
     """
     build tree either by read new data or read saved data
     """
     saved_tree_path = 'save/pypy_tree.pkl'
 
     if not should_build_new and os.path.isfile(saved_tree_path):
+        print('Loading Tree From File:', saved_tree_path)
         with open(saved_tree_path, 'rb') as file_input:
             tree = pickle.load(file_input)
-    else:        
+    else:
+        if(not should_build_new):
+            print('=======', 'No saved file found', '=======')
+
+        print('Building Tree From', voronoi_file_source)
         voronoi_list = input_reader.read_input(voronoi_file_source)
         tree = RTree(max_content_size = 4)
 
         start_construct = time.time()
         for voronoi in voronoi_list:
             print('processing:', voronoi.name)
-            # tree.insert(voronoi)
-            #  tree.reset()
-            #  tree.inspectTree()
-            #  print(tree.choose_leaf())    
+            tree.insert(voronoi)
+            
         duration = time.time() - start_construct
         print('Tree Construction Time (ms):', duration)
 
         with open(saved_tree_path, 'wb') as file_output:
+            print('Saving Tree To:', saved_tree_path)
             pickle.dump(tree, file_output, pickle.HIGHEST_PROTOCOL)
         
     return tree
 
-
 if __name__ == '__main__':
-
-    FILE_INPUT = 'test/test-region-4.input'	
-    TREE, ALL_VORONOI = build_tree(FILE_INPUT, should_build_new = True)
-
-    # with open('save/pypy_seq-05-titik.pkl', 'wb') as file_output:
-    #     pickle.dump(tree, file_output, pickle.HIGHEST_PROTOCOL)
-
-    # for v in voronoi_list:
-    #     print(v.name)
-    # points = [  Point(), #R0
-    #             Point(1.000, 198),#R14
-    #             Point(662.1,551.96)]#R8
-    # for p in points:
-
-    # for v in voronoi_list;
-
-    # random.uniform(1.0,)
+    FILE_INPUT = 'test/region-4/region-4.input'	
+    TREE = build_tree(FILE_INPUT, should_build_new = False)
 
     """
     Input query point
     """
-
     QUERY_POINT = Point(465,484)
 
     # """
